e4ff721862ccb93730656abceded5825c51c7600
Jenna Tan <jennajltan@gmail.com>
2022-06-28 18:38:42 +0100

    Edited top2vec.py, roberta.py

--- NLP_sentiment/create_top2vec.py
+++ NLP_sentiment/create_top2vec.py
@@ -1,8 +1,8 @@
-# !pip install top2vec==1.0.16
-# !pip install tensorflow tensorflow_hub tensorflow_text
-# !pip install 'top2vec[sentence_encoders]'
-# !pip install 'top2vec[sentence_transformers]'
-# !pip install 'top2vec[indexing]'
+#!pip install top2vec==1.0.16
+#!pip install tensorflow tensorflow_hub tensorflow_text
+#!pip install 'top2vec[sentence_encoders]'
+#!pip install 'top2vec[sentence_transformers]'
+#!pip install 'top2vec[indexing]'
 
 import pandas as pd
 from top2vec import Top2Vec
@@ -18,18 +18,21 @@ class CreateTop2Vec():
         self.speed = speed
         self.min_count = min_count
         self.embedding_model = embedding_model
+        self.model=None
+        print('Model instantiated')
 
     def run_model(self):
-        list_tweets_2 = [str(i) for i in df['preprocessed']]
-        model = Top2Vec(list_tweets_2, self.workers, self.speed, self.min_count, self.embedding_model)
-        print(model.__dict__)
-        return model
+        list_tweets = [str(i) for i in df['preprocessed']]
+        self.model= Top2Vec(list_tweets, workers=self.workers, speed=self.speed, min_count=self.min_count, embedding_model=self.embedding_model)
+        print(self.model.__dict__)
+        return self.model
 
-    def generate_wordcloud(self, model, search_term):
-        topic_words, word_scores, topic_scores, topic_nums = model.search_topics(keywords=[search_term], num_topics=5)
-        return model.generate_topic_wordcloud(topic_num=topic_nums[0], background_color='white')
+    def generate_wordcloud(self, search_term):
+        topic_words, word_scores, topic_scores, topic_nums = self.model.search_topics(keywords=[search_term], num_topics=5)
+        print('Wordcloud generated')
+        return self.model.generate_topic_wordcloud(topic_num=topic_nums[0], background_color='white')
 
 if __name__ == "__main__":
-    top2vec_practice = CreateTop2Vec(min_count=150)
+    top2vec_practice = CreateTop2Vec()
     top2vec_practice.run_model()
-    top2vec_practice.generate_wordcloud(top2vec_practice.run_model(), "migrant")
+    top2vec_practice.generate_wordcloud( "muslim")
